0ca127f45aa3b616e62de49c0b1bd80ce79f197b
msenejoa <michaelsenejoa@gmail.com>
2017-08-23 11:11:46 -0400

    updated profile information

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -18,7 +18,7 @@ const Profile = props => {
                     <li><a className="fa fa-github fa-2x" href={'https://github.com/'+profileObj.profiles[1].username}></a></li>
                   </ul>
                   <div className="divider"></div>
-                  <p>I built this site with <a href="https://facebook.github.io/react/">React</a> components and a <a href="https://jsonresume.org/schema/">JSON Resume Schema</a>. The full source code can be found in <a href="https://github.com/freaksauce/React-Resume-ES6">my Github repo</a>.</p>
+                  <p>This site was built with <a href="https://facebook.github.io/react/">React</a> components and a <a href="https://jsonresume.org/schema/">JSON Resume Schema</a>. The full source code can be found in <a href="https://github.com/msenejoa/msenejoa.github.io">my Github repo</a>.</p>
             </div>
 };
 
